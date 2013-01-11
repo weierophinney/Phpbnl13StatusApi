daf6d1599fe927351ee0ba271671e39971fc6140
Matthew Weier O'Phinney <matthew@zend.com>
2013-01-11 16:10:25 -0600

    Created validator for status objects
    - Validates that required fields are present for each type

---
+++ src/Phpbnl13StatusApi/StatusValidator.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Phpbnl13StatusApi;
+
+class StatusValidator
+{
+    public function isValid(StatusInterface $status)
+    {
+        $user = $status->getUser();
+        if (!is_string($user) || empty(trim($user))) {
+            return false;
+        }
+
+        $type = $status->getType();
+        switch ($type) {
+            case StatusInterface::TYPE_STATUS:
+                return $this->validateStatus($status);
+                break;
+            case StatusInterface::TYPE_IMAGE:
+                return $this->validateImage($status);
+                break;
+            case StatusInterface::TYPE_LINK:
+                return $this->validateLink($status);
+                break;
+            default:
+                return false;
+        }
+    }
+
+    public function validateStatus(StatusInterface $status)
+    {
+        $text = $status->getText();
+        if (!is_string($text) || empty(trim($text))) {
+            return false;
+        }
+        return true;
+    }
+
+    public function validateImage(StatusInterface $status)
+    {
+        $url = $status->getImageUrl();
+        if (!is_string($url) || empty(trim($url))) {
+            return false;
+        }
+        return true;
+    }
+
+    public function validateLink(StatusInterface $status)
+    {
+        $url = $status->getLinkUrl();
+        if (!is_string($url) || empty(trim($url))) {
+            return false;
+        }
+        return true;
+    }
+}
