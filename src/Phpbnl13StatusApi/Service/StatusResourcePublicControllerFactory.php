a16bcf5ec9fd7b50c8685c26101a45441a6ba681
Matthew Weier O'Phinney <matthew@zend.com>
2013-01-18 10:38:52 -0600

    Updated due to change in ResourceController
    - s/setHttpOptions/setResourceHttpOptions/

--- src/Phpbnl13StatusApi/Service/StatusResourcePublicControllerFactory.php
+++ src/Phpbnl13StatusApi/Service/StatusResourcePublicControllerFactory.php
@@ -21,7 +21,7 @@ class StatusResourcePublicControllerFactory implements FactoryInterface
         $controller->setResource($resource);
         $controller->setPageSize($pageSize);
         $controller->setRoute('phpbnl13_status_api/public');
-        $controller->setHttpOptions(array('GET'));
+        $controller->setResourceHttpOptions(array('GET'));
         return $controller;
     }
 }
